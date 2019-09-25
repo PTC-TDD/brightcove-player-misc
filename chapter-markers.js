537d4e4f66d8908bc6abdf8517871189d82e66d6
TDD <42146501+PTC-TDD@users.noreply.github.com>
2019-09-25 16:20:23 -0400

    Update chapter-markers.js

--- chapter-markers.js
+++ chapter-markers.js
@@ -1,13 +1,55 @@
-var player;
 videojs.registerPlugin('chapterMarkers', function() {
-    player = this;
-    var overlay = document.createElement('p');
+    var myPlayer = this,
+      videoDuration,
+      cuesAra = [],
+      chapterStartTimesAra = [];
+    myPlayer.on('loadedmetadata', function() {
+      // +++  Use the array filter function to retrieve data structure that contains chapter cue points +++
+      chapterTT = [].filter.call(videojs.players.myPlayerID.textTracks(), function(tt) {
+        return tt.kind === 'chapters';
+      });
+      //  Retrieve actual array of chapter cue points
+      cuesAra = chapterTT[0].cues;
+  
+      // +++ Loop over chapter cue points and get start time of each  +++
+      for (var i = 0; i < cuesAra.length; i++) {
+        chapterStartTimesAra[i] = cuesAra[i].startTime;
+      }
+  
+      // +++ Call function to create marks in progress bar  +++
+      // Get the video duration
+      videoDuration = myPlayer.mediainfo.duration;
+      // Call the function to add the marks in the progress control
+      addMarkers(chapterStartTimesAra, videoDuration);
+    });
+  
+    // +++ Add chapter markers +++
+    function addMarkers(cuePointsAra, videoDuration) {
+      var iMax = cuePointsAra.length,
+        i,
+        playheadWell = document.getElementsByClassName('vjs-progress-control vjs-control')[0];
+      // Loop over each cue point, dynamically create a div for each
+      // then place in div progress bar
+      for (i = 0; i < iMax; i++) {
+        var elem = document.createElement('div');
+        elem.className = 'vjs-marker';
+        elem.id = 'cp' + i;
+        elem.style.left = (cuePointsAra[i] / videoDuration) * 100 + '%';
+        console.log('elem.style.left', elem.style.left);
+        playheadWell.appendChild(elem);
+      }
+    }
+  });
+
+/* videojs.registerPlugin('pluginDev', function() {
+    var player = this,
+    overlay = document.createElement('p');
     overlay.className = 'vjs-overlay';
     overlay.innerHTML = "Becoming a plugin developer";
     player.el().appendChild(overlay);
   });
-
-videojs.getPlayer("vjs_video_3").on("loadedmetadata", function() {
+  
+  videojs.getPlayer("myPlayerID").on("loadedmetadata", function() {
     var myPlayer = this,
       videoDuration,
       cuesAra = [],
@@ -51,4 +93,4 @@ videojs.getPlayer("vjs_video_3").on("loadedmetadata", function() {
       console.log("elem.style.left", elem.style.left);
       playheadWell.appendChild(elem);
     }
-  }
+  }*/
